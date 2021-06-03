474ba649ccddb42b0acf0640ae562b19253c503c
Dimitar Tsvetanov <25179264+cvetanovv@users.noreply.github.com>
2021-06-03 21:24:03 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  3 20:43:45 2021
 
-@author: cvetanovv
-"""
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
